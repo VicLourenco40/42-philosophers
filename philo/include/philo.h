33b167223cf50294af094d39a10aad768cb54502
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-06-26 17:40:43 +0100

    Remove philosopher status and mutex

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,14 +6,13 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 12:00:45 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/06/26 14:07:31 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/06/26 17:04:30 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <sys/time.h>
 # include <pthread.h>
 # include <stddef.h>
 
@@ -30,35 +29,19 @@ typedef struct s_params
 
 typedef struct s_fork
 {
-	int				taken;
+	int				user;
 	pthread_mutex_t	mutex;
 }	t_fork;
-
-typedef enum e_status
-{
-	THINKING,
-	EATING,
-	SLEEPING
-}	t_status;
 
 typedef struct s_philo
 {
 	int				index;
-	t_status		status;
 	t_fork			*forks[2];
 	int				num_meals;
 	long			last_meal;
-	pthread_mutex_t	mutex;
 	pthread_t		thread;
 	t_params		*params;
 }	t_philo;
-
-typedef struct s_state
-{
-	t_params	params;
-	t_philo		*philos;
-	t_fork		*forks;
-}	t_state;
 
 void	*ft_calloc(size_t nmemb, size_t size);
 int		ft_atoi(const char *nptr);
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,20 +6,27 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 12:01:14 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/06/26 14:37:05 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/06/26 17:37:48 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void	*philosopher(void *arg)
 {
 	t_philo *const	philo = arg;
 
 	return (NULL);
+}
+
+void	manager(t_params *const params, t_philo *const philos)
+{
+	int	i;
+
+	i = -1;
+	while (++i < params->num_philos)
+		pthread_join(philos[i].thread, NULL);
 }
 
 void	init_params(t_params *const params, const int num_args,
@@ -36,41 +43,38 @@ void	init_params(t_params *const params, const int num_args,
 	pthread_mutex_init(&params->mutex, NULL);
 }
 
-int	init_state(t_state *const state, const int num_args,
+int	init(t_params *const params, t_philo **const philos, const int num_args, \
 	char *const *const args)
 {
-	t_params *const	params = &state->params;
-	int				i;
+	t_fork	*forks;
+	int		i;
 
 	init_params(params, num_args, args);
-	state->philos = ft_calloc(params->num_philos, \
-		sizeof(t_philo) + sizeof(t_fork));
-	if (!state->philos)
+	*philos = ft_calloc(params->num_philos, sizeof(t_philo) + sizeof(t_fork));
+	if (!*philos)
 		return (1);
-	state->forks = (t_fork *)&state->philos[params->num_philos];
+	forks = (t_fork *)&(*philos)[params->num_philos];
 	i = -1;
 	while (++i < params->num_philos)
 	{
-		state->philos[i].index = i + 1;
-		state->philos[i].forks[0] = &state->forks[i];
-		state->philos[i].forks[1] = &state->forks[i + 1];
-		if (i + 1 == params->num_philos)
-			state->philos[i].forks[1] = state->forks;
-		state->philos[i].last_meal = get_timestamp();
-		state->philos[i].params = params;
-		pthread_mutex_init(&state->philos[i].mutex, NULL);
-		pthread_mutex_init(&state->forks[i].mutex, NULL);
-		pthread_create(&state->philos[i].thread, NULL,
-			philosopher, &state->philos[i]);
+		(*philos)[i].index = i + 1;
+		(*philos)[i].forks[0] = &forks[i];
+		(*philos)[i].forks[1] = &forks[(i + 1) % params->num_philos];
+		(*philos)[i].last_meal = get_timestamp();
+		(*philos)[i].params = params;
+		pthread_mutex_init(&forks[i].mutex, NULL);
+		pthread_create(&(*philos)[i].thread, NULL, philosopher, &philos[i]);
 	}
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
-	t_state	state;
+	t_params	params;
+	t_philo		*philos;
 
-	if (argc < 5 || argc > 6 || init_state(&state, argc, argv))
+	if (argc < 5 || argc > 6 || init(&params, &philos, argc, argv))
 		return (1);
-	free(state.philos);
+	manager(&params, philos);
+	free(philos);
 }
